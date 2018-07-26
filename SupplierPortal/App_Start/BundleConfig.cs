ddd09d35ff48aa4847a5311ad10f0a9ab459a6e1
zhangyanxin <zhang0xin@gmail.com>
2018-07-26 11:13:36 +0800

    1.0

--- SupplierPortal/App_Start/BundleConfig.cs
+++ SupplierPortal/App_Start/BundleConfig.cs
@@ -5,26 +5,21 @@ namespace SupplierPortal
 {
   public class BundleConfig
   {
-    // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                   "~/Scripts/jquery-{version}.js"));
-
       bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                   "~/Scripts/jquery-ui-{version}.js"));
-
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                   "~/Scripts/jquery.unobtrusive*",
                   "~/Scripts/jquery.validate*"));
 
       // 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
       // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
+      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
       bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                   "~/Content/themes/base/jquery.ui.core.css",
                   "~/Content/themes/base/jquery.ui.resizable.css",
@@ -38,6 +33,14 @@ namespace SupplierPortal
                   "~/Content/themes/base/jquery.ui.datepicker.css",
                   "~/Content/themes/base/jquery.ui.progressbar.css",
                   "~/Content/themes/base/jquery.ui.theme.css"));
+
+      // bootstrap 
+      bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+        "~/Content/bootstrap/js/jquery.js",
+        "~/Content/bootstrap/js/bootstrap.js"
+        ));
+      bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+        "~/Content/bootstrap/css/bootstrap.css"));
     }
   }
 }
